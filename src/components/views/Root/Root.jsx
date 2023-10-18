1ad94464c8adb6437629242b1a6774be5a51d982
dzymenko georgy <erterj05@gmail.com>
2023-10-19 02:55:11 +0300

    .

--- src/components/views/Root/Root.jsx
+++ src/components/views/Root/Root.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { store, persistor } from '../../../app/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-import { HashRouter as RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { CalendarView } from '../CalendarView/CalendarView';
 import { ScheduleView } from '../ScheduleView/ScheduleView';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <CalendarView/>,
