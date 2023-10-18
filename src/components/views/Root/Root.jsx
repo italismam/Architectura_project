13cb7b67e868995f4519fcddff668b68314af753
dzymenko georgy <erterj05@gmail.com>
2023-10-19 02:39:24 +0300

    basename for router was updated

--- src/components/views/Root/Root.jsx
+++ src/components/views/Root/Root.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { store, persistor } from '../../../app/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-import { Outlet, Route, Router, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements, useNavigate } from 'react-router-dom';
+import { Outlet, Route, Router, HashRouter as RouterProvider, Routes, createBrowserRouter, createRoutesFromElements, useNavigate } from 'react-router-dom';
 import { CalendarView } from '../CalendarView/CalendarView';
 import history from '../../../utils/history';
 import { ScheduleView } from '../ScheduleView/ScheduleView';
@@ -22,7 +22,7 @@ export const Root = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+        <RouterProvider basename="/cinema_calendar" router={router} />
       </PersistGate>
     </Provider>
   )
